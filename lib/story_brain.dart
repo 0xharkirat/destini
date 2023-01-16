c840f3ce00d214d099408631cf874410ccac5215
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2023-01-16 19:18:24 +1100

    Step 8/9/10 - Display the Story in the App

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,7 +1,7 @@
-//TODO: Step 6 - import the story.dart file into this file.
+
 import 'story.dart';
 
-//TODO: Step 5 - Create a new class called StoryBrain.
+
 class StoryBrain{
 
   List<Story> _storyData = [
@@ -30,16 +30,20 @@ class StoryBrain{
          '')
   ];
 
+  String getStory(){
+    return _storyData[0].storyTitle;
+  }
+
 
 }
 
-//TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
+
 
 
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
-//TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
+
 
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
