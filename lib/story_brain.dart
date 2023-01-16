f3faec7a4398f2a114849c87928718afeefb0e9b
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2023-01-16 19:36:49 +1100

    Step 21 - Story flowchart

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -45,6 +45,33 @@ class StoryBrain{
   }
 
   void nextStory(int choiceNumber){
+    // StoryNumber = 0
+    if (_storyNumber == 0){
+      if( choiceNumber == 1){
+        _storyNumber = 2;
+      }
+      else if ( choiceNumber == 2){
+        _storyNumber = 1;
+      }
+    }
+    // StoryNumber = 1
+    else if (_storyNumber == 1){
+      if ( choiceNumber == 1){
+        _storyNumber = 2;
+      }
+      else if (choiceNumber == 2){
+        _storyNumber = 3;
+      }
+    }
+  //  StoryNumber = 2
+    else if (_storyNumber == 2){
+      if (choiceNumber == 1){
+        _storyNumber = 5;
+      }
+      else if (choiceNumber == 2){
+        _storyNumber = 4;
+      }
+    }
 
   }
 
@@ -59,9 +86,6 @@ class StoryBrain{
 
 
 
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
 
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
